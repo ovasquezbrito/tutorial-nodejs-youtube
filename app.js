f5f5ee9939c67143ea8c03fdcf4fdb8d3155c1df
OduberVasquez <ovasquezcyt@gmail.com>
2020-10-25 15:54:00 -0400

    Primeras clases

---
+++ app.js
@@ -0,0 +1,15 @@
+const {frutas, dinero} = require('./frutas')
+
+const cowsay = require('cowsay')
+
+console.log(cowsay.say({
+	text : "Esto es una vaca",
+	e : "oO",
+	T : "U "
+}));
+
+/* frutas.forEach(item => {
+    console.count(item)
+}) */
+
+console.log(dinero)
---
+++ frutas.js
@@ -0,0 +1,7 @@
+const frutas = ['platano', 'manzana', 'platano', 'pera']
+const dinero = 2000
+
+module.exports = {
+    frutas,
+    dinero
+}
